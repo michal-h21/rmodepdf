4a09a1ae49f0f1721195127498238450030e9635
Michal Hoftich <michal.h21@gmail.com>
2020-10-25 22:40:19 +0100

    save images

--- src/rmodepdf-htmllib.lua
+++ src/rmodepdf-htmllib.lua
@@ -1,4 +1,5 @@
 local domobject = require "luaxml-domobject"
+
 -- download content of URL
 local function curl(url)
   local command = io.popen("curl -sS ".. url,"r")
@@ -6,6 +7,15 @@ local function curl(url)
   local content = command:read("*all")
   command:close()
   return content
+end
+
+local function get_mimetype(url)
+  local command = io.popen("curl -sS -I " .. url,"r")
+  local content = command:read("*all")
+  command:close()
+  status = tonumber(content:match("^%S+%s+(%d+)"))
+  mimetype = content:match("content%-type:%s*([%a%/]+)")
+  return status, mimetype
 end
 
 local function parse_metadatafile(metadatafile)
@@ -81,12 +91,60 @@ local function tidy(tmpfile)
 end
 
 
-local function download_images(contents)
+local mime_to_ext = {
+  ["image/png"] = "png",
+  ["image/jpeg"] = "jpg"
+}
+
+local function hash_img_name(imgdir, url, mimetype)
+  -- normalize imgdir
+  local imgdir = imgdir:match("/$") and imgdir or imgdir .. "/"
+  local extension = mime_to_ext[ mimetype ] 
+  if not extension then return nil, "Cannot find extension for mimetype: " .. mimetype end
+  -- md5 should be enough for this purpose
+  local hash = md5.sumhexa(url)
+  return imgdir .. hash .. "." .. extension
+end
+
+local function download_images(contents, imgdir)
+  local dom = domobject.parse(contents)
+  local images = dom:query_selector("img")
+  -- stop process if the page doesn't contain any images
+  if #images == 0 then return contents end
+  -- create 
+  local status, msg = lfs.mkdir(imgdir)
+  for _, img in ipairs(images) do
+    local src = img:get_attribute("src")
+    local status, mimetype = get_mimetype(src)
+    -- process only accessible images
+    if status == 200 then
+      -- we want to hash image names, in order to save them to the destination directory
+      local newname = hash_img_name(imgdir, src, mimetype)
+      if newname then
+        -- don't save the same image multiple times
+        -- test if the file already exists
+        local status = io.open(newname, "r")
+        if status then
+          status:close()
+        else
+          local f = io.open(newname, "w")
+          -- load image from server and save it to a local file
+          local content = curl(src)
+          if content then f:write(content) end
+          f:close()
+          print(src, status, mimetype, newname, status)
+        end
+        img:set_attribute("src", newname)
+      end
+    end
+  end
+  return dom:serialize()
 end
 
 return {
   curl = curl,
   readability = readability,
   tidy = tidy,
+  download_images = download_images,
 
 }
