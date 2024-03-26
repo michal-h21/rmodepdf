7481b97c8b9afe60a713b4a9e2dc83bba9cf98ca
Michal Hoftich <michal.h21@gmail.com>
2024-03-26 15:33:43 +0100

    tmp file library done

--- src/rmodepdf-tmpfiles.lua
+++ src/rmodepdf-tmpfiles.lua
@@ -1,1 +1,29 @@
-return {}
+
+local tmpfiles = {}
+
+local function register_tmpname(name)
+  -- register tmp name, so it can be removed later
+  tmpfiles[#tmpfiles+1] = name
+end
+
+local function tmpname()
+  -- return tmp file name and register it in the list of tmp files
+  local name = os.tmpname()
+  register_tmpname(name)
+  return name
+end
+
+local function clean()
+  -- remove all registered tmp files
+  for _, name in ipairs(tmpfiles) do
+    print("removing", name)
+    os.remove(name)
+  end
+end
+
+return {
+  tmpname = tmpname,
+  register_tmpname = register_tmpname,
+  clean = clean,
+}
+
