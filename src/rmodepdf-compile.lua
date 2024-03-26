9542072a1c1dc11e509296f5b23702d4460bee0f
Michal Hoftich <michal.h21@gmail.com>
2024-03-26 14:44:59 +0100

    added compile library

---
+++ src/rmodepdf-compile.lua
@@ -0,0 +1,34 @@
+
+-- this library provides log parsing function
+local error_logparser = require "make4ht-errorlogparser"
+
+
+local function run_latex(content, jobname)
+  local devnull =  "> /dev/null 2>&1" 
+  if os.type == "windows" then
+    devnull = " > nul 2>&1"
+  end
+  local cmd = io.popen("lualatex --interaction=bathmode -j " .. jobname .. " " .. devnull, "w")
+  cmd:write(content)
+  return {cmd:close()}
+end
+
+local function test_log_file(jobname)
+  local log_file_name = jobname .. ".log"
+
+  
+end
+
+
+
+
+
+local function compile(content, jobname)
+  local res = run_latex(content, jobname)
+  print("res: " .. res[3])
+end
+
+
+return {
+  compile = compile
+}
