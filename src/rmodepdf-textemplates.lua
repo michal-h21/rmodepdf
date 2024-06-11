d08e64761b01a6ca5aac124c02d839ea149a61f2
Michal Hoftich <michal.h21@gmail.com>
2024-06-11 16:33:27 +0200

    added conditional templates and dot syntax

--- src/rmodepdf-textemplates.lua
+++ src/rmodepdf-textemplates.lua
@@ -2,7 +2,31 @@
 --- @module 'textemplates'
 local textemplates = {}
 
---- expand
+--- remove braces
+--- @param text string braced text
+local function remove_braces(text)
+  return text:gsub("^{", ""):gsub("}$","")
+end
+
+--- find if table contains a value. multidimensional values can be specified, separated by dot
+---@param text string dot separated tree of values
+---@param params table to be searched
+local function get_value(text, params)
+  -- get value before dot and rest of the string
+  local key, rest = text:match("([^%.]+)%.?(.*)")
+  local value = params[key]
+  if value then
+    if rest and rest ~= ""  then
+      -- try to find next key in the current subtable
+      return get_value(rest, value)
+    else
+      return value
+    end
+  end
+  return nil
+end
+
+--- expand template
 ---@param template string
 ---@param content string
 ---@param params table
@@ -10,9 +34,22 @@ local textemplates = {}
 function textemplates.expand(template, content, params)
   local content = content or ""
   local params = params or {}
-  -- first, expand variables from params
-  local result = template:gsub("@{(..*)}", function(key)
-    return params[key] or ""
+  -- test if the variable in the first parameter exists and expand true or false part accordingly
+  local result = template:gsub("%?(%b{})(%b{})(%b{})", function(test, true_part, false_part)
+    local test, true_part, false_part = remove_braces(test), remove_braces(true_part), remove_braces(false_part)
+    local value = get_value(test, params)
+    if value then
+      return textemplates.expand(true_part, content, params)
+    else
+      return textemplates.expand(false_part, content, params)
+    end
+  end)
+  --  expand variables from params
+  result = result:gsub("@(%b{})", function(key)
+    local key = remove_braces(key)
+    -- return original text if the value is empty
+    if not key or key == "" then return "@{}" end
+    return get_value(key, params) or ""
   end)
   result = result:gsub("%%s", content)
   return result
--- test/textemplates.lua
+++ test/textemplates.lua
@@ -12,4 +12,46 @@ describe("Basic expansion should work", function()
   it("Should ignore empty parameter", function ()
     assert.same(expand("hello @{}"), "hello @{}")
   end)
+  it("Should be OK with longer texts", function ()
+    local test = 
+    [[\usepackage[@{lang}]{babel}
+    \setmainfont{Literata}
+    \usepackage[@{geometry}]{geometry}
+    \pagestyle{@{pagestyle}}]]
+    print(expand(test, "", {lang="czech"}))
+
+  end)
+end)
+
+describe("Dot separated values should be find in subtables", function()
+  local test_table = {
+    hello = "world,",
+    subtable = {
+      second = "here we are",
+      another = {
+        third = "even here"
+      }
+    }
+  }
+  it("Should find values in subtables", function()
+    assert.same(expand("where we are? @{subtable.second}","", test_table), "where we are? here we are")
+    assert.same(expand("where we are? @{subtable.another.third}","", test_table), "where we are? even here")
+  end)
+end)
+
+describe("Conditional templates should work", function ()
+  local test_table = {
+    hello = "world"
+  }
+  it("Should expand basic conditions", function ()
+    assert.same(expand("hello ?{hello}{exists}{doesn't exist}", "", test_table), "hello exists")
+    assert.same(expand("nothello ?{nothello}{exists}{doesn't exist}", "", test_table), "nothello doesn't exist")
+  end)
+  it("Should handle TeX code", function()
+    assert.same(expand("hello ?{hello}{\\textit{exists}}{doesn't exist}", "", test_table), "hello \\textit{exists}")
+  end)
+  it("Should expand variables in conditions", function ()
+    assert.same(expand("what? ?{hello}{@{hello} exists}{doesn't exist}", "", test_table), "what? world exists")
+
+  end)
 end)
